e86599313a22c647f5e4e0fb95428274bc3924e7
Kevin Torres Martínez <torresmtzkevin@gmail.com>
2021-03-24 12:54:34 -0600

    CodeChef - COOK127C: WEIGHTBL & PAWRI.

---
+++ CodeChef/PAWRI.cpp
@@ -0,0 +1,44 @@
+//
+//  Pawri Meme
+//  March Cook-Off 2021 Division 3
+//
+//  Created by Kevin Torres Martínez on 24/3/21.
+//  Copyright © 2021 Kevin Torres Martínez. All rights reserved.
+//
+//  Round: https://www.codechef.com/COOK127C?order=desc&sortBy=accuracy
+//  Problem: https://www.codechef.com/COOK127C/problems/PAWRI
+//
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+  int t;
+  cin >> t;
+  cin.ignore(256, '\n');
+
+  for (int k = 0; k < t; k++)
+  {
+    string str;
+    getline(cin, str);
+
+    int pos = 0;
+    while (pos < str.length())
+    {
+      pos = str.find("party", pos);
+
+      if (pos != string::npos)
+      {
+        str.replace(str.begin() + pos + 2, str.begin() + pos + 5, "wri");
+        pos += 1;
+      }
+    }
+
+    cout << str << endl;
+  }
+
+  return 0;
+}
---
+++ CodeChef/WEIGHTBL.cpp
@@ -0,0 +1,35 @@
+//
+//  Weight Balance
+//  March Cook-Off 2021 Division 3
+//
+//  Created by Kevin Torres Martínez on 24/3/21.
+//  Copyright © 2021 Kevin Torres Martínez. All rights reserved.
+//
+//  Round: https://www.codechef.com/COOK127C?order=desc&sortBy=accuracy
+//  Problem: https://www.codechef.com/COOK127C/problems/WEIGHTBL
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int t;
+  cin >> t;
+
+  for (int k = 0; k < t; k++)
+  {
+    int w1, w2, x1, x2, M;
+    cin >> w1 >> w2 >> x1 >> x2 >> M;
+
+    int ans = 0,
+        minIncrease = M * x1,
+        maxIncrease = M * x2,
+        difference = w2 - w1;
+
+    cout << ((difference >= minIncrease && difference <= maxIncrease) ? 1 : 0) << endl;
+  }
+
+  return 0;
+}
